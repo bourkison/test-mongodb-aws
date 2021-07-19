aeb1131e09a9def049c498ab5bcb887e8bb97d50
bourkison <harrisonbourke97@gmail.com>
2021-07-20 00:49:14 +1000

    Fixed MongoDB URI

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const MongoClient = require("mongodb").MongoClient;
 
 // Define our connection string. Info on where to get this will be described below. In a real world application you'd want to get this string from a key vault like AWS Key Management, but for brevity, we'll hardcode it in our serverless function here.
 const MONGODB_URI =
-  "mongodb+srv://harrisonbourke:L7bTz89OHHU8haRw@projectburncluster.vvmny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://harrisonbourke:L7bTz89OHHU8haRw@projectburncluster.vvmny.mongodb.net/ProjectBurnDatabase?retryWrites=true&w=majority";
 
 // Once we connect to the database once, we'll store that connection and reuse it so that we don't have to connect to the database on every request.
 let cachedDb = null;
